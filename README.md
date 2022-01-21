b9d759ed0adedf723f82aa0487f53d49ccf4716b
AfaqJaved <afaqjavedofficial@gmail.com>
2022-01-21 22:24:09 +0500

    added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Svelte App With Tailwind
+
+Sevelte App
+version=3.0.0
+
+Tailwind
+version=3.0.15
+
+# Created And Maintained By AfaqJaved
---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<div class="flex w-screen h-screen justify-center gap-5 items-center flex-col bg-indigo-300 ">
+  <img src="svlete_logo.png" alt="Logo" class="w-20 h-20" />
+  <h1 class="text-4xl text-center ">Welcome To Svelte With TailwindCSS</h1>
+  <p>Created And Maintained By AfaqJaved</p>
+</div>
